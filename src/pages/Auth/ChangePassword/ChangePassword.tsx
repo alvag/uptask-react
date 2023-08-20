d460cce5b55965f0a9d127d9e76cc5cb3c53f46e
Max Alva <alva85@gmail.com>
2023-08-20 15:21:03 -0500

    Change password, confirm account

--- src/pages/Auth/ChangePassword/ChangePassword.tsx
+++ src/pages/Auth/ChangePassword/ChangePassword.tsx
@@ -4,7 +4,28 @@ interface ChangePasswordProps {}
 
 const ChangePasswordPage: FC<ChangePasswordProps> = () => {
     return (
-        <div>ChangePassword Page</div>
+        <>
+            <h1 className="text-sky-600 font-black text-6xl capitalize">Restablece tu password y no pierdas
+                acceso { ' ' }
+                <span className="text-slate-700">proyectos</span>
+            </h1>
+
+            <form className="my-10 bg-white shadow rounded-lg p-10">
+
+                <div className="my-5">
+                    <label htmlFor="password"
+                           className="uppercase text-gray-600 block text-xl font-bold">Nuevo Password</label>
+                    <input type="password" id="password" placeholder="NuevoßPassword"
+                           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"/>
+                </div>
+
+
+                <button type="submit"
+                        className="bg-sky-700 hover:bg-sky-900 mt-5 cursor-pointer w-full py-3 text-white uppercase font-bold rounded transition-colors">
+                    Restablecer password
+                </button>
+            </form>
+        </>
     );
 };
 
