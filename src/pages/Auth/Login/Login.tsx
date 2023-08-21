58b22e82ce31713379cdbc6ca37f0ee3e1223bc4
Max Alva <alva85@gmail.com>
2023-08-21 09:25:52 -0500

    Login

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,19 +1,58 @@
-import { FC } from 'react';
+import { FC, FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Alert, AlertProps } from '@/components';
+import { ApiError } from '@/services/client-http';
+import { login } from '@/services';
 
 interface LoginProps {}
 
 const LoginPage: FC<LoginProps> = () => {
+    const [ email, setEmail ] = useState( '' );
+    const [ password, setPassword ] = useState( '' );
+    const [ alert, setAlert ] = useState<AlertProps | null>( null );
+
+    const handleSubmit = async ( e: FormEvent<HTMLFormElement> ) => {
+        e.preventDefault();
+
+        try {
+            setAlert( null );
+
+            if ( [ email, password ].includes( '' ) ) {
+                setAlert( {
+                    message: 'Todos los campos son obligatorios',
+                    type: 'error',
+                } );
+            }
+
+            const { token, user } = await login( email, password );
+            localStorage.setItem( 'token', token );
+            console.log( user );
+
+        } catch ( e ) {
+            const { message } = e as ApiError;
+            setAlert( {
+                message,
+                type: 'error',
+            } );
+        }
+
+    };
+
     return (
         <>
             <h1 className="text-sky-600 font-black text-6xl capitalize">Inicia sesion y administra tus { ' ' }
                 <span className="text-slate-700">proyectos</span>
             </h1>
 
-            <form className="my-10 bg-white shadow rounded-lg p-10">
+            <form className="my-10 bg-white shadow rounded-lg p-10" onSubmit={ handleSubmit }>
+
+                { alert && <Alert { ...alert } /> }
+
                 <div className="my-5">
                     <label htmlFor="email" className="uppercase text-gray-600 block text-xl font-bold">Email</label>
                     <input type="email" id="email" placeholder="Email de registro"
+                           value={ email }
+                           onChange={ ( e ) => setEmail( e.target.value ) }
                            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"/>
                 </div>
 
@@ -21,6 +60,8 @@ const LoginPage: FC<LoginProps> = () => {
                     <label htmlFor="password"
                            className="uppercase text-gray-600 block text-xl font-bold">Password</label>
                     <input type="password" id="password" placeholder="Password"
+                           value={ password }
+                           onChange={ ( e ) => setPassword( e.target.value ) }
                            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"/>
                 </div>
 
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { ClientHttp } from '@/services/client-http';
+import { User } from '@/interfaces';
 
 const path = 'auth';
 const http = new ClientHttp();
@@ -13,4 +14,13 @@ export const recoveryPassword = ( email: string ) => {
 
 export const resetPassword = ( token: string, password: string ) => {
     return http.post<{ message: string }>( `${ path }/reset-password/${ token }`, { password } );
+};
+
+interface LoginResponse {
+    token: string;
+    user: User;
+}
+
+export const login = ( email: string, password: string ) => {
+    return http.post<LoginResponse>( `${ path }/login`, { email, password } );
 };
